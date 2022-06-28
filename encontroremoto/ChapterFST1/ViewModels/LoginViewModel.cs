9c576736bc327ca24cc4e01e0b4b809493568089
Elisson Andrade <75750665+elissonx@users.noreply.github.com>
2022-06-28 10:42:34 -0300

    Create LoginViewModel.cs

---
+++ encontroremoto/ChapterFST1/ViewModels/LoginViewModel.cs
@@ -0,0 +1,13 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace ChapterFST1.ViewModels
+{
+    public class LoginViewModel
+    {
+        [Required(ErrorMessage = "E-mail Requerido")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Senha Requerida")]
+        public string Senha { get; set; }
+    }
+}
